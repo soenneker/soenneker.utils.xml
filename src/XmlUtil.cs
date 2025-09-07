94b42ea0af311ea04cc4a037748d8a9a67325750
Jake Soenneker <jake@soenneker.com>
2025-09-07 09:37:14 -0500

    Adds stream support

--- src/XmlUtil.cs
+++ src/XmlUtil.cs
@@ -14,47 +14,43 @@ namespace Soenneker.Utils.Xml;
 public static class XmlUtil
 {
     /// <summary>
-    /// Accepts a nullable object... if null returns null.
+    /// Serialize to a string (returns null if <paramref name="obj"/> is null).
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="obj"></param>
-    /// <param name="encoding"></param>
-    /// <param name="removeNamespaces"></param>
-    /// <returns></returns>
     public static string? Serialize<T>(T? obj, Encoding? encoding, bool removeNamespaces = true)
     {
-        if (obj is null)
+        encoding ??= Encoding.UTF8;
+        XDocument? doc = SerializeToXDocument(obj, encoding, removeNamespaces);
+
+        if (doc is null)
             return null;
+
+        return doc.Declaration + doc.ToString();
+    }
+
+    /// <summary>
+    /// Serialize to a stream (no-op if <paramref name="obj"/> is null).
+    /// </summary>
+    public static void Serialize<T>(T? obj, Stream destination, Encoding? encoding = null, bool removeNamespaces = true, bool leaveOpen = false)
+    {
+        if (obj is null)
+            return;
 
         encoding ??= Encoding.UTF8;
 
-        var serializer = new XmlSerializer(typeof(T));
+        XDocument doc = SerializeToXDocument(obj, encoding, removeNamespaces)!;
 
-        XmlSerializerNamespaces? namespaces = null;
-
-        if (removeNamespaces)
+        var settings = new XmlWriterSettings
         {
-            namespaces = new XmlSerializerNamespaces();
-            namespaces.Add("", "");
-        }
+            Encoding = encoding,
+            OmitXmlDeclaration = false,
+            CloseOutput = !leaveOpen,
+            Indent = false
+        };
 
-        var doc = new XDocument {Declaration = new XDeclaration("1.0", encoding.HeaderName, null)};
-
-        using (XmlWriter writer = doc.CreateWriter())
-        {
-            serializer.Serialize(writer, obj, namespaces);
-        }
-
-        // remove all nulls
-        doc.Descendants()
-            .Where(x => (bool?) x.Attribute(XName.Get("nil", "http://www.w3.org/2001/XMLSchema-instance")) == true)
-            .Remove();
-
-        string result = doc.Declaration + doc.ToString();
-
-        return result;
+        using var xw = XmlWriter.Create(destination, settings);
+        doc.WriteTo(xw);
+        xw.Flush();
     }
-
     /// <summary>
     /// Accepts a nullable object... if null returns null.
     /// </summary>
@@ -73,5 +69,66 @@ public static class XmlUtil
         }
 
         return obj;
+    }
+
+    /// <summary>
+    /// Deserializes an object of type <typeparamref name="T"/> from a stream.
+    /// Stream position will be read from its current position. Honors XML encoding/declaration automatically.
+    /// </summary>
+    /// <param name="source">Source stream containing XML.</param>
+    /// <param name="leaveOpen">Whether to leave <paramref name="source"/> open after reading.</param>
+    /// <returns>Deserialized object or default if stream is null/empty.</returns>
+    public static T? Deserialize<T>(Stream? source, bool leaveOpen = false)
+    {
+        if (source is null)
+            return default;
+
+        // If we can determine emptiness, short-circuit. If not seekable, let XmlSerializer handle it.
+        if (source.CanSeek && source.Length - source.Position == 0)
+            return default;
+
+        var xs = new XmlSerializer(typeof(T));
+
+        var xrSettings = new XmlReaderSettings
+        {
+            CloseInput = !leaveOpen
+        };
+
+        using var reader = XmlReader.Create(source, xrSettings);
+        return (T?)xs.Deserialize(reader);
+    }
+
+    /// <summary>
+    /// Core: build an <see cref="XDocument"/> with declaration, optional namespace stripping,
+    /// and remove nodes marked <c>xsi:nil="true"</c>.
+    /// </summary>
+    private static XDocument? SerializeToXDocument<T>(T? obj, Encoding encoding, bool removeNamespaces)
+    {
+        if (obj is null)
+            return null;
+
+        var serializer = new XmlSerializer(typeof(T));
+
+        XmlSerializerNamespaces? ns = null;
+        if (removeNamespaces)
+        {
+            ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+        }
+
+        var doc = new XDocument { Declaration = new XDeclaration("1.0", encoding.HeaderName, null) };
+
+        using (XmlWriter writer = doc.CreateWriter())
+        {
+            serializer.Serialize(writer, obj, ns);
+        }
+
+        // Strip xsi:nil elements
+        XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+        doc.Descendants()
+            .Where(e => (bool?)e.Attribute(xsi + "nil") == true)
+            .Remove();
+
+        return doc;
     }
 }
